7f97c311f849f7df431415ebf5001c6ce2981f28
Theresa-o <okorotheresa19@gmail.com>
2022-09-13 20:01:34 +0100

    updated requirements

--- project3/settings.py
+++ project3/settings.py
@@ -11,13 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import configparser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-conf = configparser.ConfigParser()
-conf.read(os.path.join(BASE_DIR, 'settings.ini'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +25,7 @@ SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dexmail-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
