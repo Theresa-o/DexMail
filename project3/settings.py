9bc098122b8485bbed1a921093c3c387c53a3e75
Theresa-o <okorotheresa19@gmail.com>
2022-09-13 07:15:54 +0100

    Updated settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dexmail-production.up.railway.app/']
+ALLOWED_HOSTS = ['dexmail-production.up.railway.app']
 
 
 # Application definition
@@ -121,6 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
